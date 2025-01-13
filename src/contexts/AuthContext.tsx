e5af82fbfb379d8a92b5eff25dd8475e57f37450
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-01-13 03:32:33 +0100

    dodaje useAuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from 'react';
+import useAuth, { UseAuthResult } from '../hooks/useAuth.ts';
+import useSubscriptions from '../hooks/useSubscriptions.ts';
+
+const AuthContext = createContext<UseAuthResult | null>(null);
+
+export const AuthProvider = ({ children }: any) => {
+  const auth = useAuth();
+  useSubscriptions(auth.accessToken);
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export const useAuthContext = () => useContext(AuthContext);
