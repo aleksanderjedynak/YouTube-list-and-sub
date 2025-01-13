555134e728e215da7603b926eb038c100813c3fe
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-01-13 03:32:09 +0100

    App dodanie Contextu oraz Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Footer from './Footer.tsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import ListView from './components/ListView';
+import Subscriptions from './components/Subscriptions';
+import { AuthProvider } from './contexts/AuthContext';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        To jest prosty starter do stawiania lokalnie z vite react i deploy na
-        Vercela
-      </p>
-      <Footer />
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/subscriptions" element={<Subscriptions />} />
+          <Route path="/:listName" element={<ListView />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
